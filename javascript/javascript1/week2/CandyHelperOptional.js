e9bc49d5a26796413b77cd84ea817895b43ff3d0
slastushevskyi <slastushevskyi@gmail.com>
2022-12-04 09:55:41 +0100

    modified function, now it works

--- javascript/javascript1/week2/CandyHelperOptional.js
+++ javascript/javascript1/week2/CandyHelperOptional.js
@@ -1,39 +1,39 @@
 const boughtCandyPrices = [];
-let sweet, 
-    chocolate, 
-    toffee, 
-    chewingGum,
-    totalPrice;
-
+let totalPrice;
+let sweet = "sweet";
+let chocolate = "chocolate";
+let toffee = "toffee"; 
+let chewingGum = "chewingGum";
 const amountToSpend = Math.random() * 100; 
 function addCandy(candyType,weight) {
-    if (candyType === sweet) {
+
+    if (candyType === "sweet") { 
         boughtCandyPrices.push(weight * 0.5);       
     }
-    else if (candyType === chocolate) {
+    else if (candyType === chocolate) { //now candyType working both with variable and string ¯\_(ツ)_/¯
         boughtCandyPrices.push(weight * 0.7);   
     }
-    else if (candyType === toffee) {
+    else if (candyType === "toffee") {
         boughtCandyPrices.push(weight * 1.1);   
     }
-    else if (candyType === chewingGum) {
+    else if (candyType === "chewingGum") {
         boughtCandyPrices.push(weight * 0.03);   
     }
     else {
         console.log(`Please enter valid candy name`);
     }
 }
-addCandy(sweet,20);
-addCandy(chocolate,60);
-addCandy(toffee,15);
-addCandy(chewingGum,45);
+addCandy("sweet",20);
+addCandy("chocolate",60);
+addCandy("toffee",15);
+addCandy("chewingGum",45);
+addCandy("sour", 25); //invalid candy name
 function boughtSum(boughtCandyPrices) {
     totalPrice = 0;
     for (let i = 0; i < boughtCandyPrices.length; i++) {
         totalPrice += boughtCandyPrices[i];
     }
-    console.log(totalPrice);
-   
+    console.log(totalPrice); 
 }
 function canBuyMoreCandy() {
     if (totalPrice >= amountToSpend) {
