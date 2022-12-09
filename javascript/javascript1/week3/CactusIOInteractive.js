8f4a26b83e00745732fcb462335c52c844c1c3c4
slastushevskyi <slastushevskyi@gmail.com>
2022-12-09 23:05:03 +0100

    func. showTime modified

--- javascript/javascript1/week3/CactusIOInteractive.js
+++ javascript/javascript1/week3/CactusIOInteractive.js
@@ -15,13 +15,16 @@ addActivity("Youtube", 30);
 addActivity("Movie", 170);
 addActivity("Talking with parents", 45);
 
-function showStatus(array) {
+function showStatus(array,activityDate) {
     let x = 0;
     let i;
     let n;
     if (array.length>0){
         for (i = 0; i<array.length; i++) {
             x += array[i].duration
+                if (activityDate === array[i].date) {
+                console.log(array[i])
+            }
         }
         console.log(`You have added ${i} activities. They amount to ${x} min. of usage`);
         if (x > 60) {
@@ -36,59 +39,38 @@ function showStatus(array) {
         console.log(`Add some activities before calling showStatus`)
     }
 }
+activities.push({date:"11/8/2022", activity:"Coding", duration:600});
+activities.push({date:"11/8/2022", activity:"Solving troubles", duration:60});
+activities.push({date:"10/8/2022", activity:"Reading doc-tion", duration:160});
+showStatus(activities, "11/8/2022");
 
+// I know, this method is BULLSHIT, but i can't get how to do it right.
 
-// // Improved SHOWSTATUS that showing a number of actitivies for that specific day.
-// activities.push({date:"11/8/2022", activity:"Coding", duration:600});
-// activities.push({date:"11/8/2022", activity:"Solving troubles", duration:60});
-// activities.push({date:"10/8/2022", activity:"Reading doc-tion", duration:160});
-// function showStatus(array, activityDate) {
-//     // activityDate = toString(activityDate); I need to find a way how to transform activityDate to string, so user could put date as a number.
-//     let x = 0;
-//     let i;
-//     if (array.length>0) {
-//         for (i = 0; i<array.length; i++) {
-//             x += array[i].duration
-//             if (activityDate === array[i].date) {
-//                 console.log(array[i])
+// function calcActivicties(array) {
+//         if (array[0].duration > array[1].duration) {
+//             if (array[0].duration > array[2].duration) {
+//             console.log(array[0])
 //             }
 //         }
-//         console.log(`You have added ${i} activities. They amount to ${x} min. of usage`);
-//     }
-//     else {
-//         console.log(`Add some activities before calling showStatus`)
-//     }
+//         else if (array[0].duration > array[2].duration) {
+//             if (array[0].duration > array[1].duration)
+//             console.log(array[0])
+//         }
+//         else if (array[1].duration > array[0].duration) {
+//             if (array[1].duration > array[2].duration)
+//             console.log(array[1])
+//         }
+//         else if (array[1].duration > array[2].duration) {
+//             if (array[1].duration > array[0].duration)
+//             console.log(array[1])
+//         }
+//         else if (array[2].duration > array[0].duration) {
+//             if (array[2].duration > array[1].duration)
+//             console.log(array[1])
+//         }
+//         else if (array[2].duration > array[1].duration) {
+//             if (array[2].duration > array[0].duration)
+//             console.log(array[1])
+//         }
 // }
-// showStatus(activities, "11/8/2022");
-
-
-
-function calcActivicties(array) {
-        if (array[0].duration > array[1].duration) {
-            if (array[0].duration > array[2].duration) {
-            console.log(array[0])
-            }
-        }
-        else if (array[0].duration > array[2].duration) {
-            if (array[0].duration > array[1].duration)
-            console.log(array[0])
-        }
-        else if (array[1].duration > array[0].duration) {
-            if (array[1].duration > array[2].duration)
-            console.log(array[1])
-        }
-        else if (array[1].duration > array[2].duration) {
-            if (array[1].duration > array[0].duration)
-            console.log(array[1])
-        }
-        else if (array[2].duration > array[0].duration) {
-            if (array[2].duration > array[1].duration)
-            console.log(array[1])
-        }
-        else if (array[2].duration > array[1].duration) {
-            if (array[2].duration > array[0].duration)
-            console.log(array[1])
-        }
-}
-calcActivicties(activities);
-
+// calcActivicties(activities);
